5cf28a0aaa0eac2fe8a2572f814c8d01ece99f30
TaushiqB <130788932+TaushiqB@users.noreply.github.com>
2024-09-21 03:21:58 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
+  //new git code
+  base: "/My-3d-website/"
 })
