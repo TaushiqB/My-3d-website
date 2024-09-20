6941e5d66b21c4183e14c5739167bb9f9ab067c3
TaushiqB <130788932+TaushiqB@users.noreply.github.com>
2024-09-21 03:09:23 +0530

    add: initial files

---
+++ src/constants/index.js
@@ -0,0 +1,223 @@
+
+import temple from '/src/assets/images/temple.png';
+import food from '/src/assets/images/food.png';
+import drink from '/src/assets/images/drinks.png';
+import movie from '/src/assets/images/movie.png';
+import memm from '/src/assets/images/memm.png';
+
+
+
+
+
+export const temples = [
+    {
+        title: "Arulmigu Subramaniya Swamy Temple🛕🙏",
+        company_name: "Marudhamalai",
+        icon: temple,
+        iconBg: "#fbc3bc",
+        date: "🕉️🛕🙏",
+        image: '/src/assets/images/marudhamalai.jpeg',
+    },
+    {
+        title: "Arulmigu Eachanari Vinayagar Temple🛕🙏",
+        company_name: "Eachanari",
+        icon: temple,
+        iconBg: "#fbc3bc",
+        date: "🕉️🛕🙏",
+        image: '/src/assets/images/eachanari temple.jpeg',
+    },
+    {
+        title: "Sri Perur Pateeswarar Temple🛕🙏",
+        company_name: "Perur",
+        icon: temple,
+        iconBg: "#fbc3bc",
+        date: "🕉️🛕🙏",
+        image: '/src/assets/images/perur temple.jpeg',
+    },
+    {
+        title: "Arulmigu Koniamman Temple🛕🙏",
+        company_name: "Ukkadam",
+        icon: temple,
+        iconBg: "#fbc3bc",
+        date: "🕉️🛕🙏",
+        image: '/src/assets/images/konniamman temple.jpeg',
+    },
+    {
+        title: "Adiyogi",
+        company_name: "Isha Yoga Centre🛕🙏",
+        icon: temple,
+        iconBg: "#fbc3bc",
+        date: "🕉️🛕🙏",
+        image: '/src/assets/images/isha.jpeg',
+    },
+    {
+        title: "Sri Naga Sai Mandir🛕🙏",
+        company_name: "Saibaba Colony",
+        icon: temple,
+        iconBg: "#fbc3bc",
+        date: "🕉️🛕🙏",
+        image: '/src/assets/images/sai temple.jpeg',
+    },
+    {
+        title: "Ramar Temple🛕🙏",
+        company_name: "Ram Nagar",
+        icon: temple,
+        iconBg: "#fbc3bc",
+        date: "🕉️🛕🙏",
+        image: '/src/assets/images/ramar temple.jpeg',
+    },
+    {
+        title: "Sree Ayyappan Temple🛕🙏",
+        company_name: "Siddhapudur",
+        icon: temple,
+        iconBg: "#fbc3bc",
+        date: "🕉️🛕🙏",
+        image: '/src/assets/images/ayyapan temple.jpeg',
+    },
+];
+
+export const foods = [
+    {
+        title: "French Fries🍽️✨",
+        company_name: "",
+        icon: food,
+        iconBg: "#accbe1",
+        date: "🍽️🍴🥂",
+        image: '/src/assets/images/french fry.jpeg',
+    },
+    {
+        title: "French Fries + Vanilla Icecream🍽️✨",
+        company_name: "",
+        icon: food,
+        iconBg: "#fbc3bc",
+        date: "🍽️🍴🥂",
+        image: '/src/assets/images/french vanilla.jpeg',
+    },
+    {
+        title: "Biryani🍽️✨",
+        company_name: "",
+        icon: food,
+        iconBg: "#b7e4c7",
+        date: "🍽️🍴🥂",
+        image: '/src/assets/images/biryani.jpeg',
+    },
+    {
+        title: "Madagascar Chocolate Icecream🍽️✨",
+        company_name: "NIC ice cream",
+        icon: food,
+        iconBg: "#a2d2ff",
+        date: "🍽️🍴🥂",
+        image: '/src/assets/images/ice cream.jpeg',
+    },
+];
+
+export const drinks = [
+    {
+        title: "Proud Lady🍹✨",
+        company_name: "Park Elanza",
+        icon: drink,
+        iconBg: "#accbe1",
+        date: "🍵☕🧃",
+        image: '/src/assets/images/lady.jpeg',
+    },
+    {
+        title: "Milkshake🍹✨",
+        company_name: "Biggies Burger",
+        icon: drink,
+        iconBg: "#fbc3bc",
+        date: "🍵☕🧃",
+        image: '/src/assets/images/biggies.jpeg',
+    },
+];
+
+export const movies = [
+    {
+        title: "Fidaa🎥✨",
+        company_name: "	Varun Tej, Sai Pallavi",
+        icon: movie,
+        iconBg: "#accbe1",
+        date: "🎥🎬🍿",
+        image: '/src/assets/images/fidaa.jpeg',
+    },
+
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "Starbucks",
+        icon: memm,
+        iconBg: "#accbe1",
+        date: "March 2020 - April 2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+
+];
+
+export const codissia = [
+    {
+        title: "Trade Fair🏰✨",
+        company_name: "codissia",
+        icon: memm,
+        iconBg: "#accbe1",
+        date: "🏰👀✨",
+        image: '/src/assets/images/codissia.jpeg',
+    },
+
+];
+
+export const nachos = [
+    {
+        title: "Nachos🌮✨",
+        company_name: "Pillar 129",
+        icon: memm,
+        iconBg: "#accbe1",
+        date: "🌮👩‍❤️‍👨✨",
+        image: '/src/assets/images/nachos.jpeg',
+    },
+
+];
+
+export const lake = [
+    {
+        title: "Lake of Love🌅✨",
+        company_name: "Ukkadam Lake",
+        icon: memm,
+        iconBg: "#accbe1",
+        date: "🌅🎇✨",
+        image: '/src/assets/images/lake.jpeg',
+    },
+
+];
+
+export const mm = [
+    {
+        title: "Vendhu Thanindhathu Kaadu🎬✨",
+        company_name: "STR",
+        icon: memm,
+        iconBg: "#accbe1",
+        date: "🎥🎬🍿",
+        image: '/src/assets/images/vtk.jpeg',
+    },
+
+];
+
+export const milkshake = [
+    {
+        title: "Chocolate Milkshake🥤✨",
+        company_name: "Blenders",
+        icon: memm,
+        iconBg: "#accbe1",
+        date: "🥤🌌✨",
+        image: '/src/assets/images/blenders.jpeg',
+    },
+
+];
+
+
+
